6bf1ad9d0163cdcc6e17cc5d0c664db7995627ef
Volker Bergmann <volker@databene.org>
2009-10-14 16:28:08 +0000

    using milliseconds time resolution

--- src/main/java/org/databene/contiperf/log/FileExecutionLogger.java
+++ src/main/java/org/databene/contiperf/log/FileExecutionLogger.java
@@ -1,3 +1,25 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU Lesser General Public License (LGPL), Eclipse Public License (EPL) 
+ * and the BSD License.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.databene.contiperf.log;
 
 import java.io.File;
@@ -35,7 +57,8 @@ public class FileExecutionLogger implements ExecutionLogger {
 
 	public void logSummary(String id, long elapsedTime, long invocationCount, long startTime) {
 		OutputStream out = null;
-        String message = id + "," + elapsedTime + ',' + invocationCount + ',' + startTime + LINE_SEPARATOR;
+        String message = id + "," + (elapsedTime / 1000000) + ',' 
+        	+ invocationCount + ',' + (startTime / 1000000) + LINE_SEPARATOR;
 		try {
 	        out = new FileOutputStream(FILENAME, true);
 	        out.write(message.getBytes());
