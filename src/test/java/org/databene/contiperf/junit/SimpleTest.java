9cd13368a53d10d35169189e9f3600960919e51a
Volker Bergmann <volker@databene.org>
2009-10-14 16:27:03 +0000

    Added new features

--- src/test/java/org/databene/contiperf/junit/SimpleTest.java
+++ src/test/java/org/databene/contiperf/junit/SimpleTest.java
@@ -1,5 +1,6 @@
 package org.databene.contiperf.junit;
 
+import org.databene.contiperf.PerfTest;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -9,8 +10,14 @@ public class SimpleTest {
 	public ContiPerfRule i = new ContiPerfRule();
 	
 	@Test
-	public void sleepASecond() throws Exception {
-		Thread.sleep(1000);
+	public void sleepAWhile() throws Exception {
+		Thread.sleep(100);
+	}
+
+	@PerfTest(invocations = 5, timeLimit = 1200)
+	@Test(timeout = 250)
+	public void sleepALittleLonger() throws Exception {
+		Thread.sleep(200);
 	}
 
 }
