82bae1242ac1b60d65ffb3365bb057f736cfd170
Volker Bergmann <volker@databene.org>
2010-03-29 10:30:44 +0000

    removed config file support for first release

--- src/main/java/org/databene/contiperf/Config.java
+++ src/main/java/org/databene/contiperf/Config.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2009-2010 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -22,19 +22,8 @@
 
 package org.databene.contiperf;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-import org.databene.contiperf.util.IOUtil;
-
 /**
- * Parses and provides file based test configuration.<br/><br/>
+ * Parses and provides the ContiPerf configuration.<br/><br/>
  * Created: 18.10.2009 06:46:31
  * @since 1.0
  * @author Volker Bergmann
@@ -43,18 +32,8 @@ public class Config {
 
 	public static final String SYSPROP_ACTIVE = "contiperf.active";
 	public static final String SYSPROP_CONFIG_FILENAME = "contiperf.config";
+	public static final String DEFAULT_CONFIG_FILENAME = "contiperf.config.xml";
 	
-	private static final String DEFAULT_CONFIG_FILENAME = "contiperf.properties";
-	
-	private Map<String, Integer> testInvocationCounts;
-	private Map<String, Integer> testTimeouts;
-	private int defaultInvocationCount;
-	
-	
-	public Config() {
-	    readConfig();
-    }
-
 	public boolean active() {
 		String sysprop = System.getProperty(SYSPROP_ACTIVE);
 		return (sysprop == null || !"false".equals(sysprop.toLowerCase()));
@@ -62,46 +41,7 @@ public class Config {
 
 	// helpers ---------------------------------------------------------------------------------------------------------
 
-	private void readConfig() {
-	    String filename = configFileName();
-    	Properties props = new Properties();
-	    InputStream in = inputStream(filename);
-	    if (in != null) {
-	    	try {
-	    		props.load(in);
-	    	} catch (IOException e) {
-	    		e.printStackTrace();
-	    	} finally {
-	    		IOUtil.close(in);
-	    	}
-	    }
-	    defaultInvocationCount = 1;
-		testInvocationCounts = new HashMap<String, Integer>();
-		for (Map.Entry<?, ?> entry : props.entrySet()) {
-	        String methodName = entry.getKey().toString();
-	        int invocationCount = Integer.parseInt(entry.getValue().toString());
-	        if ("default".equals(methodName))
-	        	defaultInvocationCount = invocationCount;
-	        else
-	        	testInvocationCounts.put(methodName, invocationCount);
-        }
-		testTimeouts = new HashMap<String, Integer>();
-		// TODO populate testTimeouts
-    }
-	
-	private static InputStream inputStream(String filename) {
-		File file = new File(filename);
-		if (file.exists()) {
-			try {
-	            return new FileInputStream(file);
-            } catch (FileNotFoundException e) {
-	            // This is not supposed to happen. But if it does, we fall back to the context ClassLoader!
-            }
-		}
-		return Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
-    }
-
-	private static String configFileName() {
+	public static String getConfigFileName() {
 		String filename = System.getProperty(SYSPROP_CONFIG_FILENAME);
 		if (filename == null || filename.trim().length() == 0)
 			filename = DEFAULT_CONFIG_FILENAME;
@@ -116,9 +56,10 @@ public class Config {
 	    return instance;
     }
 
+	@SuppressWarnings("unused") 
 	public int getInvocationCount(String testId) {
-	    Integer count = testInvocationCounts.get(testId);
-		return (count != null ? count : -1);
+		// TODO v1.1 read config file and support override of annotation settings
+		return -1;
     }
 
 }
