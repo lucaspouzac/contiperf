a7fc7ba057e5940463c4a9aed8d749419f166191
Volker Bergmann <volker@databene.org>
2011-01-18 12:26:28 +0000

    removed task

--- src/main/java/org/databene/contiperf/junit/ContiPerfSuiteRunner.java
+++ src/main/java/org/databene/contiperf/junit/ContiPerfSuiteRunner.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2010-2011 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -49,7 +49,7 @@ public class ContiPerfSuiteRunner extends Suite {
 	    try {
 	        return suiteClass.newInstance();
         } catch (Exception e) {
-	        throw new RuntimeException(e); // TODO Auto-generated catch block
+	        throw new RuntimeException(e);
         }
     }
 
