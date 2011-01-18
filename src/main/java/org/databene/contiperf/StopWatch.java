7e82b3c1a33c9c2a38fcded7d2d0a03fc39f4d55
Volker Bergmann <volker@databene.org>
2011-01-18 12:21:05 +0000

    Introduced StopWatch API

---
+++ src/main/java/org/databene/contiperf/StopWatch.java
@@ -0,0 +1,72 @@
+/*
+ * (c) Copyright 2011 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU Lesser General Public License (LGPL), Eclipse Public License (EPL) 
+ * and the BSD License.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.contiperf;
+
+import org.databene.stat.CounterRepository;
+import org.databene.stat.LatencyCounter;
+
+/**
+ * Stopwatch-style access to ContiPerf's {@link LatencyCounter} features.
+ * A StopWach is created with a name and immediately starts measuring time.
+ * When calling stop(), the elapsed time is registered at a central latency 
+ * counter identified by the stopwatch's name.
+ * <pre>
+ *     StopWatch watch = new StopWatch("mytest");
+ *     Thread.sleep(delay);
+ *     watch.stop();
+ * </pre>
+ * You can use a stop watch only a single time, so you have to create a new 
+ * instance for each measurement you are performing.
+ * After the desired number of invocations, you can query the associated 
+ * {@link LatencyCounter} from the CounterRepository and query its features, 
+ * e.g.
+ * <pre>
+ *     LatencyCounter counter = CounterRepository.getInstance("mytest");
+ *     System.out.println("avg:" + counter.averageLatency + ", max:" + counter.maxLatency())
+ * </pre>
+ * <br/><br/>
+ * Created: 14.01.2011 11:17:30
+ * @since 2.0.0
+ * @author Volker Bergmann
+ * @see CounterRepository
+ * @see LatencyCounter
+ */
+public class StopWatch {
+
+	private String name;
+	private long startTime;
+	
+	public StopWatch(String name) {
+		this.name = name;
+		this.startTime = System.nanoTime();
+	}
+	
+	public void stop() {
+		if (startTime == -1)
+			throw new RuntimeException("Called stop() on StopWatch '" + name + "' which has already been stopped");
+		int latency = (int) ((System.nanoTime() - startTime) / 1000000L);
+		startTime = -1;
+		CounterRepository.getInstance().addSample(name, latency);
+	}
+	
+}
---
+++ src/test/java/org/databene/contiperf/StopWatchTest.java
@@ -0,0 +1,88 @@
+/*
+ * (c) Copyright 2011 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU Lesser General Public License (LGPL), Eclipse Public License (EPL) 
+ * and the BSD License.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.contiperf;
+
+import static org.junit.Assert.*;
+
+import org.databene.stat.CounterRepository;
+import org.databene.stat.LatencyCounter;
+import org.junit.After;
+import org.junit.Test;
+
+/**
+ * Tests the {@link StopWatch}.<br/><br/>
+ * Created: 14.01.2011 11:33:37
+ * @since 1.08
+ * @author Volker Bergmann
+ */
+public class StopWatchTest {
+
+	private static final String NAME = "StopWatchTest";
+	
+	@After
+	public void tearDown() {
+		CounterRepository.getInstance().clear();
+	}
+	
+	@Test
+	public void testSingleCall() throws InterruptedException {
+		sleepTimed(50);
+		assertEquals(1, getCounter().sampleCount());
+	}
+
+	@Test
+	public void testSubsequentCalls() throws InterruptedException {
+		sleepTimed(50);
+		sleepTimed(50);
+		sleepTimed(50);
+		LatencyCounter counter = getCounter();
+		assertEquals(3, counter.sampleCount());
+		assertTrue(counter.minLatency() >= 39);
+		assertTrue(counter.minLatency() < 100);
+		assertTrue(counter.averageLatency() >= 39);
+		assertTrue(counter.averageLatency() < 100);
+	}
+
+	@Test
+	public void testParallelCalls() throws InterruptedException {
+		// TODO implement testParallelCalls()
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testMultiStop() {
+		StopWatch watch = new StopWatch(NAME);
+		watch.stop();
+		watch.stop();
+	}
+
+	private void sleepTimed(int delay) throws InterruptedException {
+		StopWatch watch = new StopWatch(NAME);
+		Thread.sleep(delay);
+		watch.stop();
+	}
+	
+	private LatencyCounter getCounter() {
+		return CounterRepository.getInstance().getCounter(NAME);
+	}
+
+}
