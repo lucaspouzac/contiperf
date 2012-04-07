fc9a9f32f8a19e4f1fb16b8292d08a34fb72d7e4
Volker Bergmann <volker@databene.org>
2012-04-07 14:36:22 +0000

    Javadoc

--- src/main/java/org/databene/contiperf/Required.java
+++ src/main/java/org/databene/contiperf/Required.java
@@ -31,9 +31,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Used to specify performance requirements for a test.
- * TODO general advice and examples
- * <br/><br/>
+ * Specifies performance requirements for a test.<br/><br/>
  * Created: 15.10.2009 14:42:57
  * @since 1.0
  * @author Volker Bergmann
@@ -43,31 +41,34 @@ import java.lang.annotation.Target;
 @Retention(RUNTIME)
 public @interface Required {
 	
-	/** TODO javadoc */
+	/** Requires the average number of test executions per second to be the specified value or higher. */
 	int throughput()   default -1;
 
-	/** TODO javadoc */
+	/** Requires the average test execution time to be of the specified value or less. */
 	int average()      default -1;
 	
-	/** TODO javadoc */
+	/** Requires the execution time of 50% of the test executions of the specified value or less. */
 	int median()       default -1;
 	
-	/** TODO javadoc */
+	/** Requires each test execution time of the specified value or less. */
 	int max()          default -1;
 	
-	/** TODO javadoc */
+	/** Requires the total elapsed time from the beginning of the first test execution to the end of the last one.
+	    This does not mean the accumulated response time, but the duration of the test run. */
 	int totalTime()    default -1;
 	
-	/** TODO javadoc */
+	/** Requires the execution time of 90% of the test executions of the specified value or less. */
 	int percentile90() default -1;
 
-	/** TODO javadoc */
+	/** Requires the execution time of 95% of the test executions of the specified value or less. */
 	int percentile95() default -1;
 	
-	/** TODO javadoc */
+	/** Requires the execution time of 99% of the test executions of the specified value or less. */
 	int percentile99() default -1;
 	
-	/** TODO javadoc */
+	/** Defines a custom set of percentile requirements as a comma-separated list of percentile:millisecond pairs,
+	 *  for example 80:300,96:2000 to require 80% of the invocations to take 300 ms or less and 96% to take 2000 ms 
+	 *  or less. */
 	String percentiles() default "";
 	
 }
